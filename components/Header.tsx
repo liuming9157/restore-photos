c70c4c42b36acce58bbfb190b5148f9f60d904f9
Jack Liu <liuming@mzsat.cn>
2024-06-26 11:30:12 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header({ photo }: { photo?: string | undefined }) {
           height={20}
         />
         <h1 className='sm:text-3xl text-xl font-bold ml-2 tracking-tight'>
-          restorePhotos.io
+          photo.55ai.store
         </h1>
       </Link>
       {photo ? (
