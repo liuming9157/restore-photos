c478b01438bf196ab2bf85734425c4db84924951
liuming9157 <liuming@mzsat.cn>
2024-06-27 16:20:04 +0800

    add vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 import PlausibleProvider from 'next-plausible';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <PlausibleProvider domain='restorephotos.io'>
         <Component {...pageProps} />
+        <Analytics />
       </PlausibleProvider>
     </SessionProvider>
   );
